7245abd41c93369c2283f9951f10f5a4135d7615
Per Jahn <perjahn@gmail.com>
2017-03-09 18:13:22 +0100

    1st ver

---
+++ testnunit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testnunit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new MyClass();
+            int a = args.Length == 2 ? int.Parse(args[0]) : 1;
+            int b = args.Length == 2 ? int.Parse(args[1]) : 2;
+            int result = obj.Add(a, b);
+            Console.WriteLine($"Result: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ testnunitTests/MyTestClass.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testnunit;
+
+namespace testnunitTests
+{
+    public class MyTestClass
+    {
+        [Test]
+        public void TestAdd()
+        {
+            var obj = new MyClass();
+            int a = 1;
+            int b = 2;
+            int result = obj.Add(a, b);
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
